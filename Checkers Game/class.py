cf1b09d22b8fe85fc83075a01a781489475f2bb2
AhadShah786 <60286044+AhadShah786@users.noreply.github.com>
2022-06-19 00:43:08 +0500

    Add files via upload

---
+++ Checkers Game/class.py
@@ -0,0 +1,12 @@
+from tkinter import Y
+
+def sum(x,y):
+        z=x+Y
+        return x
+    
+    
+def square(x):
+        y = x*x
+        return y
+    
+    
---
+++ Checkers Game/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+from checkers.game import Game
